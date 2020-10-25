02f2a86041dd781cd3f1de7f083fee1bf42001aa
Hiram <hirammunozr@outlook.com>
2020-10-25 16:01:44 -0600

    Added dmenu2 launch script

---
+++ dmenu/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+dmenu_run -nb '#313131' \
+          -nf '#FFFFFF' \
+          -sf $MONOKAI_BG \
+          -sb $MONOKAI_RED \
+          -l 5 \
+          -fn 'Source Code Pro:regular:pixelsize=24' \
+          -w 900 \
+          -x 510 \
+          -h 50 \
+          -y 200 
