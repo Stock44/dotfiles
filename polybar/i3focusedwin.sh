4d23cc6589046f5d76ebc03a865515a45dbc272c
Hiram Munoz <hirammunozr@outlook.com>
2022-04-18 15:21:09 -0500

    Polybar i3 focused win script now truncates the window title to 50 chars.

--- polybar/i3focusedwin.sh
+++ polybar/i3focusedwin.sh
@@ -4,7 +4,7 @@ do
     WIN_ID=$(echo $line | perl -ne '/# ([0123456789abcdef]x[0123456789abcdef]+)/ && print $1')
     if [ "$WIN_ID" != "0x0" ]; then
         CLASS=$(xprop -id $WIN_ID WM_CLASS | perl -ne '/"([^"]+)"/ && print $1')
-        TITLE=$(xprop -id $WIN_ID WM_NAME | perl -ne '/"([^"]+)"/ && print $1')
+        TITLE=$(xprop -id $WIN_ID WM_NAME | perl -ne '/"([^"]+)"/ && print $1' | cut -b1-50)
         if [ -n "$CLASS" ]; then
             printf "%%{B%s}%%{F%s}  %s %%{F%s}%s  \n" $THEME_900 $THEME_RED "$CLASS" $THEME_50 "$TITLE"
             else
