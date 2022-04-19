9a690820cba6f7720f5ffd3291e103895a9de231
Hiram Munoz <hirammunozr@outlook.com>
2022-04-19 15:33:51 -0500

    Fixed case of empty window id string on system startup

--- polybar/i3focusedwin.sh
+++ polybar/i3focusedwin.sh
@@ -2,7 +2,7 @@
 xprop -spy -root _NET_ACTIVE_WINDOW | while read line
 do
     WIN_ID=$(echo $line | perl -ne '/# ([0123456789abcdef]x[0123456789abcdef]+)/ && print $1')
-    if [ "$WIN_ID" != "0x0" ]; then
+    if [ -n "$WIN_ID" ] && [ "$WIN_ID" != "0x0" ]; then
         CLASS=$(xprop -id $WIN_ID WM_CLASS | perl -ne '/"([^"]+)"/ && print $1')
         TITLE=$(xprop -id $WIN_ID WM_NAME | perl -ne '/"([^"]+)"/ && print $1' | cut -b1-50)
         if [ -n "$CLASS" ]; then
