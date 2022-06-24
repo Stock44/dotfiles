5eba122434abcd6dec9286329a911604247ea8ba
Hiram Muñoz <hirammunozr@outlook.com>
2022-06-24 00:59:19 -0500

    Limited length for process name

--- polybar/i3focusedwin.sh
+++ polybar/i3focusedwin.sh
@@ -3,7 +3,7 @@ xprop -spy -root _NET_ACTIVE_WINDOW | while read line
 do
     WIN_ID=$(echo $line | perl -ne '/# ([0123456789abcdef]x[0123456789abcdef]+)/ && print $1')
     if [ -n "$WIN_ID" ] && [ "$WIN_ID" != "0x0" ]; then
-        CLASS=$(xprop -id $WIN_ID WM_CLASS | perl -ne '/"([^"]+)"/ && print $1')
+        CLASS=$(xprop -id $WIN_ID WM_CLASS | perl -ne '/"([^"]+)"/ && print $1' | cut -b1-20)
         TITLE=$(xprop -id $WIN_ID WM_NAME | perl -ne '/"([^"]+)"/ && print $1' | cut -b1-50)
         if [ -n "$CLASS" ]; then
             printf "%%{B%s}%%{F%s}  %s %%{F%s}%s  \n" $THEME_900 $THEME_RED "$CLASS" $THEME_50 "$TITLE"
