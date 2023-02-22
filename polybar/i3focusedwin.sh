8440e51a37d0d75ed53898ae2b8e07a816965011
Hiram Muñoz <hirammunozr@outlook.com>
2023-02-22 09:54:00 -0600

    configured more colors

--- polybar/i3focusedwin.sh
+++ polybar/i3focusedwin.sh
@@ -6,9 +6,9 @@ do
         CLASS=$(xprop -id $WIN_ID WM_CLASS | perl -ne '/"([^"]+)"/ && print $1' | cut -b1-20)
         TITLE=$(xprop -id $WIN_ID WM_NAME | perl -ne '/"([^"]+)"/ && print $1' | cut -b1-50)
         if [ -n "$CLASS" ]; then
-            printf "%%{B%s}%%{F%s}  %s %%{F%s}%s  \n" $THEME_900 $THEME_RED "$CLASS" $THEME_50 "$TITLE"
+            printf "%%{B%s}%%{F%s}  %s %%{F%s}%s  \n" $THEME_1000 $THEME_RED "$CLASS" $THEME_50 "$TITLE"
         elif [ -e "$TITLE" ]; then
-            printf "%%{B%s}%%{F%s}  %s  \n" $THEME_900 $THEME_50 "$TITLE"
+            printf "%%{B%s}%%{F%s}  %s  \n" $THEME_1000 $THEME_50 "$TITLE"
         else
             printf ""
         fi
